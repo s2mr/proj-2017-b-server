43d5889b3905e23de8baf9a0e83ce67348fcd140
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-12-14 02:52:51 +0900

    add handler qr post

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'プロジェクト演習' });
 });
 
+router.post('/api/v1/qr', function(req, res) {
+    res.send('received post request.');
+})
+
 module.exports = router;
