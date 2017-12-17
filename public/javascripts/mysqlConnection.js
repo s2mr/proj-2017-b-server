4741b48256b0e6fcf489e898fcd2e9137052be6f
Kazumasa Shimomura <kazu.devapp@gmail.com>
2017-12-17 14:51:10 +0900

    use heroku db

--- public/javascripts/mysqlConnection.js
+++ public/javascripts/mysqlConnection.js
@@ -1,11 +1,13 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 
-var dbConfig = {
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'kagaya'
-};
+// var dbConfig = {
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '',
+//   database: 'kagaya'
+// };
 
-var connection = mysql.createConnection(dbConfig);
+var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+
+// mysql.createConnection('mysql://user:pass@host/db?debug=true&charset=BIG5_CHINESE_CI&timezone=-0700');
 module.exports = connection;
